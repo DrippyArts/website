183933664b2e4acf3eabdc5ae0b72738f47a539c
DrippyArts <120247263+DrippyArts@users.noreply.github.com>
2022-12-10 14:44:29 +0530

    Add files via upload

---
+++ bruh.js
@@ -0,0 +1,9 @@
+console.log('Hey There! Love our work? Reach us out @drippy_art.69 on instagram <3');
+console.log('This Website belongs to Kislay Mishra.')
+
+var typed = new Typed(".typing", {
+    strings: ['Design Cover Arts.','Create Music Videos.', 'Design Graphics.', 'Create Logos.', 'Design Artwork.'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+})
